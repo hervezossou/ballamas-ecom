be0946d1a2084a6aed3df30dfc07a2678fcc24c9
Hervé Zossou <hervezossou8@gmail.com>
2025-07-09 14:16:23 +0100

    feat(fonts): setting up fonts for project

---
+++ src/lib/fonts.ts
@@ -0,0 +1,13 @@
+import localFont from "next/font/local";
+
+const archivoVariable = localFont({
+   src: "/fonts/Archivo-Variable.woff2",
+   variable: "--font-archivo-variable",
+   display: "swap",
+});
+
+const chillaxVariable = localFont({
+   src: "/fonts/Chillax-Variable.woff2",
+   variable: "--font-chillax-variable",
+   display: "swap",
+});
