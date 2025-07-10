5a3bf43c0753e7a8fdb28523c18542dae3989803
Hervé Zossou <hervezossou8@gmail.com>
2025-07-10 11:59:37 +0100

    update hamburger component

--- src/components/atoms/Hamburger.tsx
+++ src/components/atoms/Hamburger.tsx
@@ -1,22 +1,22 @@
-import { HamburgerProps } from "@/types"
-import Image from "next/image"
+import { HamburgerProps } from "@/types";
+import Image from "next/image";
 
 export const Hamburger = ({ isOpen, onClick }: HamburgerProps) => {
-    return (
-        <div 
-            className="flex items-center justify-center size-10 transition-all duration-300 ease-in-out cursor-pointer" 
-            onClick={onClick}
-            role="button"
-            aria-label="Toggle Menu"
-        >
-            <Image
-                src={`/icons/menu-${isOpen ? "close" : "open"}.svg`}
-                alt="Hamburger Menu"
-                width={42}
-                height={24}
-                className="w-auto h-auto"
-                priority={true}
-            />
-        </div>
-    )
-}
+   return (
+      <div
+         className="flex items-center justify-center size-10 transition-all duration-300 ease-in-out cursor-pointer"
+         onClick={onClick}
+         role="button"
+         aria-label="Toggle Menu"
+      >
+         <Image
+            src={`/icons/menu-${isOpen ? "close" : "open"}.svg`}
+            alt="Hamburger Menu"
+            width={42}
+            height={24}
+            className="w-auto h-auto"
+            priority={true}
+         />
+      </div>
+   );
+};
