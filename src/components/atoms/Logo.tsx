b58bda8c0dd0c518b726ba704ddadbf191f5c28f
Hervé Zossou <hervezossou8@gmail.com>
2025-07-10 11:59:06 +0100

    update logo component

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,17 +1,17 @@
-import { LogoProps } from "@/types"
-import Image from "next/image"
+import { LogoProps } from "@/types";
+import Image from "next/image";
 
-export const Logo = ({ variant} : LogoProps) => {
-    return (
-        <div className="flex items-center justify-center gap-2">
-            <Image
-                src={`/images/logo-${variant}.png`}
-                alt="Logo"
-                width={158}
-                height={36}
-                className="w-auto h-auto"
-                priority={true}
-            />
-        </div>
-    )
-}
+export const Logo = ({ variant }: LogoProps) => {
+   return (
+      <div className="flex items-center justify-center gap-2">
+         <Image
+            src={`/images/logo-${variant}.png`}
+            alt="Logo"
+            width={158}
+            height={36}
+            className="w-auto h-auto"
+            priority={true}
+         />
+      </div>
+   );
+};
