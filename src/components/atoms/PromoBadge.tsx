f1484d7e860f9fbff6e5dab91f7b14b1f546c17f
Hervé Zossou <hervezossou8@gmail.com>
2025-07-10 12:01:22 +0100

    feat(PromoBadge): added promobadge component

---
+++ src/components/atoms/PromoBadge.tsx
@@ -0,0 +1,23 @@
+import clsx from "clsx";
+
+interface PromoBadgeProps {
+   variant: "light" | "dark";
+}
+
+const baseStyles =
+   "w-24 h-8 flex items-center justify-center gap-2.5 px-2.5 py-2 rounded-full";
+
+const badgeStyles = {
+   light: "bg-b-white text-b-black",
+   dark: "bg-b-black text-b-white",
+};
+
+export const PromoBadge = ({ variant }: PromoBadgeProps) => {
+   return (
+      <div className={clsx(baseStyles, badgeStyles[variant])}>
+         <span className="text-xs leading-[16px] font-sans font-semibold uppercase">
+            Get Off 20%
+         </span>
+      </div>
+   );
+};
