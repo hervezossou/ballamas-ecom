87d67432750093d5c6adb1b1bcbb2da8aec507ff
Hervé Zossou <hervezossou8@gmail.com>
2025-07-10 19:05:11 +0100

    feat(CategoryItem): create category item component

---
+++ src/components/atoms/CategoryItem.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+import { Category } from "@/types";
+
+interface CategoryItemProps {
+    category: Category;
+    isSelected: boolean;
+    onSelect: (category : Category) => void;
+}
+
+export const CategoryItem = ({ category, isSelected, onSelect}: CategoryItemProps) => {
+    const selectedStyles = isSelected ? "bg-b-black text-b-white" : "bg-b-white text-b-black border-2 border-b-black";
+
+    const baseStyles = "w-max h-12 flex items-center justify-between gap-2 px-4 py-3 rounded-full cursor-pointer transition-colors duration-300 font-sans font-medium";
+
+    return(
+        <button 
+            className={clsx(baseStyles, selectedStyles)} 
+            onClick={() => onSelect(category)} 
+            type="button" 
+            aria-pressed={isSelected} 
+            aria-label={`Category ${category} ${isSelected ? "selected" : "not selected"}`}
+        >
+            <span className="text-xl">{category.label}</span>
+            <span className="text-base">{category.quantity}</span>
+        </button>
+    )
+}
