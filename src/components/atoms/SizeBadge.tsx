f55cc7e122efdb0e2ac691264a9aa66ead56057d
Hervé Zossou <hervezossou8@gmail.com>
2025-07-10 12:02:06 +0100

    feat(SizeBadge): created SizeBadge component

---
+++ src/components/atoms/SizeBadge.tsx
@@ -0,0 +1,38 @@
+import { ProductSize } from "@/types";
+import clsx from "clsx";
+import { useState } from "react";
+
+interface SizeBadgeProps {
+   size: ProductSize;
+   variant: "small" | "large";
+}
+
+export const SizeBadge = ({ size, variant }: SizeBadgeProps) => {
+   const [isSelected, setIsSelected] = useState(false);
+
+   const baseStyles = `flex items-center justify-center p-3.5 rounded-full cursor-pointer transition-all font-sans font-medium ${isSelected ? "bg-b-black text-b-white" : "bg-none text-b-black border-2 border-b-black"} duration-300 ease-in-out`;
+
+   const variantStyles = {
+      small: "w-[55px] h-[42px] text-xl leading-[28px]",
+      large: "w-[75px] h-[52px] text-2xl leading-[32px]",
+   };
+
+   return (
+      <button
+         className={clsx(baseStyles, variantStyles[variant])}
+         onClick={() => setIsSelected(!isSelected)}
+         type="button"
+         aria-pressed={isSelected}
+         aria-label={`Size ${size} ${isSelected ? "selected" : "not selected"}`}
+      >
+         <span
+            className={clsx(
+               variantStyles[variant],
+               "flex items-center justify-center"
+            )}
+         >
+            {size}
+         </span>
+      </button>
+   );
+};
