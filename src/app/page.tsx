7183e6a6c6418fba0b6af7e0c6defc8bb720b6b4
Hervé Zossou <hervezossou8@gmail.com>
2025-07-14 15:49:46 +0100

    update(home): update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { SizeSelector } from "@/components/molecules/ProductSizes";
-
 export default function Home() {
    return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-         <h1 className="text-4xl font-archivo">Archivo</h1>
+         <h1 className="text-4xl font-medium font-archivo">Archivo</h1>
          <p className="font-chillax text-4xl">Chillax</p>
-         <SizeSelector />
       </main>
    );
 }
