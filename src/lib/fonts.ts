e2df88c8203158fa31a61da73c5261b9cae57aed
Hervé Zossou <hervezossou8@gmail.com>
2025-07-14 15:37:42 +0100

    feat(fonts): resolving fonts overriding issue

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -2,12 +2,12 @@ import localFont from "next/font/local";
 
 export const archivoFont = localFont({
    src: "../../public/fonts/Archivo-Variable.woff2",
-   variable: "--font-archivo-variable",
+   variable: "--font-archivo",
    display: "swap",
 });
 
 export const chillaxFont = localFont({
    src: "../../public/fonts/Chillax-Variable.woff2",
-   variable: "--font-chillax-variable",
+   variable: "--font-chillax",
    display: "swap",
 });
