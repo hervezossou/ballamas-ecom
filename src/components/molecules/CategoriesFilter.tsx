fc6f8dfb7eddfd867b890c03f9ea876da8c2352d
Hervé Zossou <hervezossou8@gmail.com>
2025-07-14 15:41:42 +0100

    update(CategoriesFilter: added category selection logic

--- src/components/molecules/CategoriesFilter.tsx
+++ src/components/molecules/CategoriesFilter.tsx
@@ -7,22 +7,23 @@ const categories: Category[] = [
    { label: "Accessories", quantity: 13 },
    { label: "Featured", quantity: 67 },
    { label: "Unisex", quantity: 52 },
-   
 ];
 
-export const CategoriesFilter = () =>{
-    const [selectedCategory, setSelectedCategory] = useState<Category>(categories[0]);
+export const CategoriesFilter = () => {
+   const [selectedCategory, setSelectedCategory] = useState<Category>(
+      categories[0]
+   );
 
-    return (
-        <div className="flex flex-wrap gap-2.5">
-            {categories.map((category) => (
-               <CategoryItem
-                  key={category.label}
-                  category={category}
-                  isSelected={selectedCategory === category}
-                  onSelect={() => setSelectedCategory(category)}
-               />
-            ))}
-        </div>
-    )
-}
+   return (
+      <div className="flex flex-wrap gap-2.5">
+         {categories.map((category) => (
+            <CategoryItem
+               key={category.label}
+               category={category}
+               isSelected={selectedCategory === category}
+               onSelect={() => setSelectedCategory(category)}
+            />
+         ))}
+      </div>
+   );
+};
