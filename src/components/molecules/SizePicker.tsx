dd2cb94b718ceddb0529e186eed17fcf4d33c502
Hervé Zossou <hervezossou8@gmail.com>
2025-07-15 17:10:44 +0100

    update sizePicker

--- src/components/molecules/SizePicker.tsx
+++ src/components/molecules/SizePicker.tsx
@@ -5,19 +5,21 @@ import { SizeBadge } from "../atoms/SizeBadge";
 const productSizes: ProductSize[] = ["XS", "S", "M", "L", "XL"];
 
 export const SizePicker = () => {
-    const [selectedSize, setSelectedSize] = useState<ProductSize>(productSizes[0]);
+   const [selectedSize, setSelectedSize] = useState<ProductSize>(
+      productSizes[0]
+   );
 
-    return (
-        <div className="flex flex-wrap gap-2.5">
-            {productSizes.map((size) => (
-                <SizeBadge 
-                    key={size} 
-                    size={size}
-                    isSelected={selectedSize === size}
-                    onSelect={() => setSelectedSize(size)} 
-                    variant="large" 
-                />
-            ))}
-        </div>
-    )
-}
+   return (
+      <div className="flex flex-wrap gap-2.5">
+         {productSizes.map((size) => (
+            <SizeBadge
+               key={size}
+               size={size}
+               isSelected={selectedSize === size}
+               onSelect={() => setSelectedSize(size)}
+               variant="large"
+            />
+         ))}
+      </div>
+   );
+};
