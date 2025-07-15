cbebed4de414f0879f13e929f5607b0dee9dccea
Hervé Zossou <hervezossou8@gmail.com>
2025-07-15 21:36:18 +0100

    update(fonts): added export for jetbrains mono

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,4 +1,5 @@
 import localFont from "next/font/local";
+import { JetBrains_Mono } from "next/font/google";
 
 export const archivoFont = localFont({
    src: "../../public/fonts/Archivo-Variable.woff2",
@@ -9,5 +10,11 @@ export const archivoFont = localFont({
 export const chillaxFont = localFont({
    src: "../../public/fonts/Chillax-Variable.woff2",
    variable: "--font-chillax",
+   display: "swap",
+});
+
+export const jetBrainsMono = JetBrains_Mono({
+   subsets: ["latin"],
+   variable: "--font-jetbrains-mono",
    display: "swap",
 });
