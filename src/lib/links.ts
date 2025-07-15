d389d2952ff83fa0d542445fb7a1317fd843268d
Hervé Zossou <hervezossou8@gmail.com>
2025-07-15 13:09:20 +0100

    feat(links): create a file to centralize all links array

---
+++ src/lib/links.ts
@@ -0,0 +1,22 @@
+export const productLinks = [
+    { name: "Jacket", href: "#" },
+    { name: "T-Shirt", href: "#" },
+    { name: "Hoodies", href: "#" },
+    { name: "Shoes", href: "#" },
+    { name: "Sunglasses", href: "#" },
+]
+
+export const categoriesLinks = [
+    { name: "Man", href: "#" },
+    { name: "Woman", href: "#" },
+    { name: "Kids", href: "#" },
+    { name: "Gift", href: "#" },
+    { name: "New Arrival", href: "#" },
+]
+
+export const socialLinks = [
+    { name: "Instagram", href: "https://www.instagram.com" },
+    { name: "Facebook", href: "https://facebook.com" },
+    { name: "YouTube", href: "https://youtube.com" },
+    { name: "X", href: "https://x.com" },
+]
