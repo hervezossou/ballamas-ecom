074e7ff59e8f7b1b47e61e4dce3c8190b0644ee9
Hervé Zossou <hervezossou8@gmail.com>
2025-07-15 17:03:44 +0100

    feat(ProductDetails): created product details component

---
+++ src/components/organisms/ProductDetails.tsx
@@ -0,0 +1,91 @@
+import Image from "next/image";
+import { useState } from "react";
+import { SizePicker } from "../molecules/SizePicker";
+import { ColorBox } from "../atoms/ColorBox";
+import { Button } from "../atoms/Button";
+
+import { Product } from "@/types";
+import { ProductColor } from "@/types";
+
+interface ProductDetailsProps {
+   product: Product;
+}
+
+export const ProductDetails = ({ product }: ProductDetailsProps) => {
+   const colors: ProductColor[] = ["ocean", "olive", "purple", "green"];
+   const [selectedColor, setSelectedColor] = useState<ProductColor>(colors[0]);
+
+   return (
+      <div
+         id={product.id}
+         className="flex flex-col items-center justify-center gap-8 p-8 lg:flex-row lg:gap-16"
+      >
+         <div className="flex flex-col items-center justify-center gap-4 rounded-4xl lg:w-1/2">
+            <Image
+               src={product.featuredImage.url}
+               alt={`${product.title} image`}
+               width={674}
+               height={672}
+            />
+         </div>
+         <div className="flex flex-col items-start justify-between gap-4 lg:w-1/2 lg:gap-7">
+            <div className="flex flex-col gap-4 lg:gap-5">
+               <div className="flex flex-col gap-2 md:gap-3 lg:gap-4.5">
+                  <h3 className="text-3xl font-chillax font-semibold md:text-[42px]">
+                     {product.title}
+                  </h3>
+                  <p className="text-b-black text-2xl font-archivo font-semibold md:text-4xl">
+                     {product.price.currencyCode} ${product.price.amount}
+                  </p>
+                  <div className="flex flex-col gap-2 lg:gap-3">
+                     <h3 className="text-xl font-archivo font-medium text-b-black md:text-3xl">
+                        Color:{" "}
+                        <span className="capitalize">{selectedColor}</span>
+                     </h3>
+                     <div className="flex items-center justify-start gap-4">
+                        {colors.map((color) => (
+                           <ColorBox
+                              key={color}
+                              color={color as ProductColor}
+                              isSelected={selectedColor === color}
+                              onSelect={(color) =>
+                                 setSelectedColor(color as ProductColor)
+                              }
+                           />
+                        ))}
+                     </div>
+                  </div>
+                  <div className="flex flex-col gap-2 lg:gap-3">
+                     <h3 className="text-xl font-archivo font-medium text-b-black md:text-3xl">
+                        Size:
+                     </h3>
+                     <SizePicker />
+                  </div>
+               </div>
+               <div className="flex items-center gap-2">
+                  <Button
+                     variant="filled"
+                     color="black"
+                     label="BUY NOW"
+                     size="large"
+                  />
+                  <Button
+                     variant="outlined"
+                     color="white"
+                     label="ADD TO CART"
+                     size="large"
+                  />
+               </div>
+            </div>
+            <div className="flex flex-col items-start justify-start gap-2 lg:gap-3.5">
+               <h3 className="text-b-black text-2xl font-medium font-chillax md:text-3xl lg:font-semibold lg:text-3xl">
+                  Description
+               </h3>
+               <p className="text-b-dark-gray text-justify text-sm md:text-lg">
+                  {product.description}
+               </p>
+            </div>
+         </div>
+      </div>
+   );
+};
