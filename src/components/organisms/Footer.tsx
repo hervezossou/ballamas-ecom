868bde3ff13b30c040e96ee792a7e1838a3626b7
Hervé Zossou <hervezossou8@gmail.com>
2025-07-16 20:06:50 +0100

    update(Footer): added use client directive

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,5 +1,6 @@
+"use client"
+
 import Link from "next/link";
-import Image from "next/image";
 import { useState } from "react";
 import { InputField } from "../atoms/InputField";
 import { Button } from "../atoms/Button";
