3b75b4e55016aa0a5e5b3b3ce805275078d8d96a
Hervé Zossou <hervezossou8@gmail.com>
2025-07-17 09:34:16 +0100

    update(Checkbox): crafted checkbox component

--- src/components/atoms/Checkbox.tsx
+++ src/components/atoms/Checkbox.tsx
@@ -1,3 +1,33 @@
-export const Checkbox = () => {
-   return <div className="flex items-center justify-center p-2"></div>;
+import clsx from "clsx";
+import Image from "next/image";
+
+interface CheckboxProps {
+   checked: boolean;
+   onClick: () => void;
+   className?: string;
+}
+
+export const Checkbox = ({ checked, className, onClick }: CheckboxProps) => {
+   const baseStyles =
+      "size-3.5 flex items-center justify-center p-0.75 border border-b-gray rounded-sm cursor-pointer lg:size-4.5 lg:p-0.5";
+   const variantStyles = `${checked ? "bg-b-black" : "bg-transparent"} transition-all duration-300 ease-in-out`;
+
+   return (
+      <button
+         className={clsx(baseStyles, variantStyles, className)}
+         role="button"
+         aria-label={checked ? "button checked" : "button not checked"}
+         onClick={onClick}
+      >
+         {checked && (
+            <Image
+               src="/icons/check-icon.svg"
+               alt="Check icon"
+               width={8}
+               height={6}
+               className="size-auto"
+            />
+         )}
+      </button>
+   );
 };
