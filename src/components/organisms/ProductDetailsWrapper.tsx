7bd6e738125d8882ed6776a6cf7d9729acf14980
Hervé Zossou <hervezossou8@gmail.com>
2025-07-17 16:06:47 +0100

    feat(ProductDetailsWrapper): create a wrapper for product details

---
+++ src/components/organisms/ProductDetailsWrapper.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { getProductById } from "@/lib/products";
+import { Product } from "@/types";
+import { ProductDetails } from "./ProductDetails";
+import { buildShopifyGlobalId } from "@/utils/buildShopifyGlobalId";
+
+interface ProductCardWrapperProps {
+  id: string;
+}
+
+export const ProductDetailsWrapper = ({ id }: ProductCardWrapperProps) => {
+  const [product, setProduct] = useState<Product | null>(null);
+  const globalId = buildShopifyGlobalId(id, "Product");
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const data = await getProductById(globalId);
+      setProduct(data);
+    }
+
+    fetchProduct();
+  }, [id]);
+
+  if (!product) return <div>Chargement...</div>;
+
+  return <ProductDetails product={product} />;
+};
