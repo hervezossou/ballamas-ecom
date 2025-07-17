61724e53de85dcccf8aa228223a8c7d5ba67c884
Hervé Zossou <hervezossou8@gmail.com>
2025-07-17 16:02:15 +0100

    feat(products): create utility functions to handle api calls

---
+++ src/lib/products.ts
@@ -0,0 +1,52 @@
+import { graphqlClient } from "./graphqlClient";
+import { gql } from "graphql-request";
+import { Product, RawProduct } from "@/types";
+import { mapProduct } from "@/utils/mapProduct";
+
+export type GetProductByIdResponse = {
+   product: RawProduct;
+};
+
+export async function getProductById(id: string): Promise<Product | null> {
+   const query = gql`
+      query GetProductById($id: ID!) {
+         product(id: $id) {
+            id
+            title
+            description
+            variants(first: 1) {
+               edges {
+                  node {
+                     price {
+                        amount
+                        currencyCode
+                     }
+                  }
+               }
+            }
+            images(first: 1) {
+                edges {
+                    node {
+                        url
+                    }
+                }
+            }
+         }
+      }
+   `;
+
+   try {
+      const variables = { id };
+      const response = await graphqlClient.request<GetProductByIdResponse>(
+         query,
+         variables
+      );
+
+      //if (!response.product) return null;
+
+      return mapProduct(response.product);
+   } catch (error) {
+      console.error("Failed to fetch product:", error);
+      return null;
+   }
+}
