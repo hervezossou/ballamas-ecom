c227538666e74687dd54853155caba927f9633e4
Hervé Zossou <hervezossou8@gmail.com>
2025-07-18 11:42:12 +0100

    update(ProductCardSkeleton): updated layout

--- src/components/skeletons/ProductCardSkeleton.tsx
+++ src/components/skeletons/ProductCardSkeleton.tsx
@@ -2,16 +2,14 @@
 
 export const ProductCardSkeleton = () => {
    return (
-      <div
-         className="w-full flex flex-col gap-4 cursor-pointer animate-pulse lg:w-[332.67px]"
-      >
-         <div className="relative h-[405px] flex items-center justify-center rounded-4xl bg-gray-200">
-            <div className="absolute inset-0 bg-gray-300 rounded-4xl" />
-         </div>
+      <div className="w-full flex flex-col gap-4 cursor-pointer animate-pulse lg:w-[332.67px]">
+         {/*Product Featured Image */}
+         <div className="h-[405px] flex items-center justify-center rounded-4xl bg-gray-300"></div>
 
+         {/* Product Details */}
          <div className="flex flex-col gap-2">
-            <div className="h-6 w-3/4 bg-gray-300 rounded-lg" /> {/* Titre */}
-            <div className="h-5 w-1/4 bg-gray-300 rounded-lg" /> {/* Prix */}
+            <div className="h-8 w-3/4 bg-gray-300 rounded-lg" /> {/* Title */}
+            <div className="h-8 w-1/5 bg-gray-300 rounded-lg" /> {/* Price */}
          </div>
       </div>
    )
