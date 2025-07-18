c4facb8ff02ad71ff6b117a07922a9c96543f3f0
Hervé Zossou <hervezossou8@gmail.com>
2025-07-18 17:52:14 +0100

    update(graphqlClient): remove console.log line

--- src/lib/graphqlClient.ts
+++ src/lib/graphqlClient.ts
@@ -2,6 +2,4 @@ import { GraphQLClient } from "graphql-request";
 
 const endpoint = process.env.NEXT_PUBLIC_GRAPHQL_API_URL as string;
 
-console.log("GRAPHQL endpoint:", endpoint);
-
 export const graphqlClient = new GraphQLClient(endpoint);
