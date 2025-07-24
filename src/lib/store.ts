5b844b5a618e46ea764b6474f57503c8ad4eccf1
Hervé Zossou <hervezossou8@gmail.com>
2025-07-24 18:31:57 +0100

    feat(store): format file

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,11 +1,12 @@
 import { create } from "zustand";
 
 type CategoryStore = {
-    selectedCategory: string;
-    setSelectedCategory: (category: string) => void;
-}
+   selectedCategory: string;
+   setSelectedCategory: (category: string) => void;
+};
 
 export const useCategoryStore = create<CategoryStore>((set) => ({
-    selectedCategory: "all",
-    setSelectedCategory: (category: string) => set({ selectedCategory: category }),
-}))
+   selectedCategory: "all",
+   setSelectedCategory: (category: string) =>
+      set({ selectedCategory: category }),
+}));
