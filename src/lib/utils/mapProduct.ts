744e3446b76f37d295f3cb9d622f4de2cef6fa6d
Hervé Zossou <hervezossou8@gmail.com>
2025-07-24 18:31:22 +0100

    feat(mapProduct): format file

--- src/lib/utils/mapProduct.ts
+++ src/lib/utils/mapProduct.ts
@@ -1,20 +1,19 @@
 import { Product, RawProduct } from "@/types";
 
 export function mapProduct(raw: RawProduct): Product {
-  const priceNode = raw.variants.edges[0]?.node.price;
-  const imageNode = raw.images.edges[0]?.node;
+   const priceNode = raw.variants.edges[0]?.node.price;
+   const imageNode = raw.images.edges[0]?.node;
 
-  return {
-    id: raw.id,
-    title: raw.title,
-    description: raw.description,
-    price: {
-      amount: Number(priceNode?.amount || 0),
-      currencyCode: priceNode?.currencyCode || "CAD",
-    },
-    featuredImage: {
-      url: imageNode?.url || "",
-    },
-  };
+   return {
+      id: raw.id,
+      title: raw.title,
+      description: raw.description,
+      price: {
+         amount: Number(priceNode?.amount || 0),
+         currencyCode: priceNode?.currencyCode || "CAD",
+      },
+      featuredImage: {
+         url: imageNode?.url || "",
+      },
+   };
 }
-
