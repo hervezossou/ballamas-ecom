4f49b698b174aaddd9805c8d5007fad76e140203
Hervé Zossou <hervezossou8@gmail.com>
2025-07-26 14:02:41 +0100

    feat(collections): create collections route and page

---
+++ src/app/collections/[handle]/page.tsx
@@ -0,0 +1,8 @@
+export default function CollectionProductsPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen py-8 lg:py-16">
+      <h1 className="font-chillax">Featured</h1>
+      <p className="font-archivo">Explore our diverse featured products.</p>
+    </div>
+  );
+}
---
+++ src/app/collections/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useState, useEffect, use } from "react";
+import { CollectionCard } from "@/components/molecules/CollectionCard";
+import { CollectionCardSkeleton } from "@/components/skeletons/CollectionCardSkeleton";
+import { Collection } from "@/types";
+import { getAllCollections } from "@/lib/products";
+
+export default function CollectionsPage() {
+  const [collections, setCollections] = useState<Collection[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCollections = async () => {
+      try {
+        setIsLoading(true);
+        const fetchedCollections = await getAllCollections(10);
+        if (fetchedCollections) {
+          setCollections(fetchedCollections || []);
+        }
+      } catch (error) {
+        console.error("Error fetching collections:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchCollections();
+  }, []);
+
+  return (
+    <>
+        <div className="w-full max-w-[1200px] flex flex-col items-start justify-between h-14 pt-8 px-10">
+            <h1 className="font-archivo font-bold text-2xl lg:text-4xl">
+              Collections
+            </h1>
+            <p className="font-archivo text-sm md:text-base lg:text-lg">
+              Explore our diverse collections of products.
+            </p>
+        </div>
+        <div className="mx-auto w-full max-w-[1200px] grid grid-cols-1 items-center justify-center gap-4 py-12 md:grid-cols-2 md:gap-8 lg:grid-cols-3 lg:gap-12 lg:py-16">
+            {isLoading ? 
+              Array.from({ length: 8 }).map((_, index) => (
+                <CollectionCardSkeleton key={index} />
+              )) :collections.map((collection) => (
+                <CollectionCard key={collection.handle} collection={collection} />
+            ))}
+        </div>
+    </>
+  );
+}
