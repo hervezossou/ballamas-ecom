9d26f9684a37b7e9476c370fffe365ecafd73cbb
Hervé Zossou <hervezossou8@gmail.com>
2025-07-28 19:22:15 +0100

    update(cart): used CartContainer, OderSummary to design cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,26 +1,47 @@
 "use client"
 
 import { useCartStore } from "@/lib/store"
+import { Button } from "@/components/atoms/Button";
+import { useRouter } from "next/navigation";
+import { OrderSummary } from "@/components/molecules/OrderSummary";
+import { CartContainer } from "@/components/organisms/CartContainer";
+import { CartItem } from "@/types";
 
 export default function CartPage() {
-    const { cartItems } = useCartStore();
-    const totalItems: number = cartItems.length;
+    const router = useRouter();
+
+    const cartItems = useCartStore((state) => state.cartItems);
+    const totalItems = cartItems.length;
+    const isEmpty:boolean = totalItems === 0;
+
+    const handleGoToShop = () => {
+        router.push("/collections");
+    };
     
     return (
         <>
-            <div className="w-full max-w-[1200px] flex flex-col items-center justify-between gap-8 min-h-screen p-4 xl:flex-row xl:items-start xl:justify-between xl:gap-12 xl:py-12">
-                <section>
-                    <h1 className="text-2xl font-bold mb-4">
-                        Cart({totalItems})
-                    </h1>
-                    {/* Here you would map through cart items and display them */}
-                </section>
-                <section>
-                    <h3 className="text-2xl font-bold mb-4">
-                        Order Summary
-                    </h3>
-                </section>
-            </div>
+            {isEmpty ? 
+                <div className="container flex flex-col items-center justify-center mt-40 text-center">
+                    <h2 className="font-archivo font-bold text-2xl mb-4 md:text-4xl xl:text-6xl">
+                        Your cart is empty
+                    </h2>
+                    <p className="text-b-dark-gray my-2 text-sm md:text-base">
+                        🛒 Looks like you haven’t added anything yet.
+                    </p>
+                    <Button 
+                        className="mt-4 px-10 py-6" 
+                        label="Go to Shop"
+                        variant="filled"
+                        color="black"
+                        size="small"
+                        onClick={handleGoToShop}
+                    />
+                </div>
+            :   <div className="container w-full max-w-[1200px] flex flex-col items-center justify-between gap-2 min-h-screen py-20 lg:flex-row lg:items-start xl:justify-between lg:gap-4">
+                    <CartContainer />
+                    <OrderSummary />
+                </div>
+            }
         </>
     )
 }
