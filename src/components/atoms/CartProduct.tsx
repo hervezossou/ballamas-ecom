f84378deccf7502798df96ccf352a737eed7c4be
Hervé Zossou <hervezossou8@gmail.com>
2025-07-28 17:18:38 +0100

    feat(cart): add CartItemCard component to display product name, size, color, and unit price

---
+++ src/components/atoms/CartProduct.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { ProductSize } from "@/types";
+import { mapSize } from "@/lib/utils/mapsize";
+
+interface CartProductProps {
+    title: string;
+    image: string;
+    color: string;
+    size: ProductSize;
+    price: number;
+}
+
+export const CartProduct = ({ title, image, color, size, price }: CartProductProps) => {
+    return (
+        <div className="w-full max-w-[308px] h-18 flex items-start gap-2.5">
+            <div className="w-18 h-18 flex items-center justify-center">
+                <Image 
+                    src={image}
+                    alt={title}
+                    width={60}
+                    height={60}
+                    className="size-18 object-cover rounded-lg"
+                    aria-description="Product image"
+                />
+            </div>
+            <div className="flex flex-col justify-between w-full max-w-[226px] h-15">
+                <span className="font-archivo font-semibold text-xs md:text-sm">
+                    {title}
+                </span>
+                <span className="font-archivo font-medium text-xs text-b-dark-gray">
+                    {color.charAt(0).toUpperCase() + color.slice(1)}-{mapSize(size)}
+                </span>
+                <span className="font-archivo font-semibold text-xs md:text-sm">
+                    ${price.toFixed(2)}
+                </span>
+            </div>
+        </div>
+    )
+}
