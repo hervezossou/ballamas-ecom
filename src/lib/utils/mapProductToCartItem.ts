599595c91447a422447f86ddc4347ca0d748a8db
Hervé Zossou <hervezossou8@gmail.com>
2025-07-29 13:21:11 +0100

    fix(utils): fix color type in map productToCartItem function

--- src/lib/utils/mapProductToCartItem.ts
+++ src/lib/utils/mapProductToCartItem.ts
@@ -1,23 +1,23 @@
-import { Product, CartItem, ProductSize } from "@/types"
+import { Product, CartItem, ProductSize, ProductColor } from "@/types";
 
 export const mapProductToCartItem = (
-    product: Product, 
-    quantity: number, 
-    size: ProductSize, 
-    color: string
+   product: Product,
+   quantity: number,
+   size: ProductSize,
+   color: ProductColor
 ): CartItem => {
-    return {
-        handle: product.handle,
-        title: product.title,
-        color,
-        size,
-        price: {
-            amount: Number(product.price.amount),
-            currency: product.price.currencyCode,
-        },
-        featuredImage: {
-            url: product.featuredImage.url
-        },
-        quantity
-    }
-}
+   return {
+      handle: product.handle,
+      title: product.title,
+      color,
+      size,
+      price: {
+         amount: Number(product.price.amount),
+         currency: product.price.currencyCode,
+      },
+      featuredImage: {
+         url: product.featuredImage.url,
+      },
+      quantity,
+   };
+};
