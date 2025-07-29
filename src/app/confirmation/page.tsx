fe709b0da8e5bf9b9b06003b9324fc40299daf34
Hervé Zossou <hervezossou8@gmail.com>
2025-07-29 15:34:56 +0100

    update(ConfirmationPage): fix svg classes issues

--- src/app/confirmation/page.tsx
+++ src/app/confirmation/page.tsx
@@ -2,10 +2,10 @@
 
 export default function ConfirmationPage() {
     return (
-        <div className="flex flex-col items-center justify-center my-40 gap-2.5">
+        <div className="h-screen flex flex-col items-center justify-center gap-2.5">
             <div className="flex items-center justify-center size-[49px] border-3 border-b-b-black p-0.5 rounded-full">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="15" viewBox="0 0 20 15" fill="none">
-                    <path d="M18 1.5L7 13.5L2 8.04545" stroke="#1D1D1D" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M18 1.5L7 13.5L2 8.04545" stroke="#1D1D1D" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
             </div>
             <div className="flex flex-col items-center justify-center gap-1">
