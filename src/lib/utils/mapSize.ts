2a1853ae577ea372ddd434cb045374cb414cfb4b
Hervé Zossou <hervezossou8@gmail.com>
2025-07-29 13:15:23 +0100

    format(mapSize): format code

--- src/lib/utils/mapSize.ts
+++ src/lib/utils/mapSize.ts
@@ -1,16 +1,16 @@
 import { ProductSize } from "@/types";
 
 export const mapSize = (size: ProductSize): string => {
-    switch (size) {
-        case "XS":
-            return "Extra Small";
-        case "S":
-            return "Small";
-        case "M":
-            return "Medium";
-        case "L":
-            return "Large";
-        case "XL":
-            return "Extra Large";
-    }
-}
+   switch (size) {
+      case "XS":
+         return "Extra Small";
+      case "S":
+         return "Small";
+      case "M":
+         return "Medium";
+      case "L":
+         return "Large";
+      case "XL":
+         return "Extra Large";
+   }
+};
