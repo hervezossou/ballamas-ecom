f4cf137bb8b77f8646a94099ef0d204da8418b9c
Hervé Zossou <hervezossou8@gmail.com>
2025-07-30 11:17:30 +0100

    feat(pages): create a signel component to handle the client side logic of product details page

---
+++ src/components/pages/ProductPage.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
+import { ProductDetailsWrapper } from "@/components/organisms/ProductDetailsWrapper";
+import { CollectionProducts } from "@/components/organisms/CollectionProducts";
+
+export default function ProductPage() {
+   const params = useParams();
+   const productHandle = params.handle as string;
+
+   return (
+      <>
+         {/* Product details page content goes here */}
+         <section className="w-full flex items-center justify-center pt-10 lg:pt-16">
+            <ProductDetailsWrapper handle={productHandle} />
+         </section>
+         <section className="w-full max-w-[1200px] flex flex-col items-start justify-between gap-4 py-8 lg:py-12 lg:gap-8">
+            <h2 className="font-chillax font-semibold text-2xl lg:text-3xl">
+               You may also like
+            </h2>
+            <div className="w-full flex self-stretch flex-col items-center justify-between gap-2 md:flex-row md:overflow-hidden lg:gap-3.5">
+               <CollectionProducts />
+            </div>
+         </section>
+      </>
+   );
+}
