ca7fd210f7091e5e9c2a90d53874a27c7e553d19
Hervé Zossou <hervezossou8@gmail.com>
2025-07-30 11:16:54 +0100

    refactor(ProductPage): split product page UI into a separated component and added metadata

--- src/app/products/[handle]/page.tsx
+++ src/app/products/[handle]/page.tsx
@@ -1,28 +1,31 @@
-"use client";
+import ProductPage from "@/components/pages/ProductPage";
+import { getProductByHandle } from "@/lib/products";
 
-import { useParams } from "next/navigation";
+export async function generateMetadata({
+   params,
+}: {
+   params: { handle: string };
+}) {
+   const product = await getProductByHandle(params.handle);
 
-import { ProductDetailsWrapper } from "@/components/organisms/ProductDetailsWrapper";
-import { CollectionProducts } from "@/components/organisms/CollectionProducts";
+   if (!product) {
+      return {
+         title: "Product Not Found",
+         description: "This product does not exist.",
+      };
+   }
 
-export default function ProductDetailsPage() {
-   const params = useParams();
-   const productHandle = params.handle as string;
+   return {
+      title: `${product.title} | Ballamas`,
+      description: product.description || "Buy now at the best price!",
+      openGraph: {
+         title: product.title,
+         description: product.description,
+         images: [product.featuredImage.url],
+      },
+   };
+}
 
-   return (
-      <>
-         {/* Product details page content goes here */}
-         <section className="w-full flex items-center justify-center pt-10 lg:pt-16">
-            <ProductDetailsWrapper handle={productHandle} />
-         </section>
-         <section className="w-full max-w-[1200px] flex flex-col items-start justify-between gap-4 py-8 lg:py-12 lg:gap-8">
-            <h2 className="font-chillax font-semibold text-2xl lg:text-3xl">
-               You may also like
-            </h2>
-            <div className="w-full flex self-stretch flex-col items-center justify-between gap-2 md:flex-row md:overflow-hidden lg:gap-3.5">
-               <CollectionProducts />
-            </div>
-         </section>
-      </>
-   );
+export default function Page() {
+   return <ProductPage />;
 }
