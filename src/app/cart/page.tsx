882b90dfffb33e15a93157fe414f5fcccd1ab0d5
Hervé Zossou <hervezossou8@gmail.com>
2025-07-30 12:10:56 +0100

    update(cart): added metadata and cart page ui component

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,49 +1,11 @@
-"use client";
+import { CartPage } from "@/components/pages/CartPage";
+import { Metadata } from "next";
 
-import { useCartStore } from "@/lib/store";
-import { Button } from "@/components/atoms/Button";
-import { useRouter } from "next/navigation";
-import { OrderSummary } from "@/components/molecules/OrderSummary";
-import { CartContainer } from "@/components/organisms/CartContainer";
+export const metadata: Metadata = {
+   title: "Your Cart | Ballamas",
+   description: "See products in your cart and checkout now.",
+};
 
-export default function CartPage() {
-   const router = useRouter();
-
-   const cartItems = useCartStore((state) => state.cartItems);
-   const totalItems = cartItems.length;
-   const isEmpty: boolean = totalItems === 0;
-
-   const handleGoToShop = () => {
-      router.push("/collections");
-   };
-
-   return (
-      <>
-         {isEmpty ? (
-            <div className="flex flex-col items-center justify-center gap-20 mt-40 text-center">
-               <div className="flex flex-col items-center gap-1 h-10">
-                  <h2 className="font-archivo font-bold text-2xl md:text-4xl xl:text-6xl">
-                     Your cart is empty!
-                  </h2>
-                  <p className="text-b-dark-gray text-sm md:text-base">
-                     🛒 Looks like you haven't added anything yet.
-                  </p>
-               </div>
-               <Button
-                  className="px-10 py-6"
-                  label="Go to Shop"
-                  variant="filled"
-                  color="black"
-                  size="small"
-                  onClick={handleGoToShop}
-               />
-            </div>
-         ) : (
-            <div className="mx-auto w-full flex flex-col items-center justify-center gap-10 py-10 lg:mx-0 xl:flex-row xl:items-start xl:py-15 xl:justify-between">
-               <CartContainer />
-               <OrderSummary />
-            </div>
-         )}
-      </>
-   );
+export default function Page() {
+   return <CartPage /> ;
 }
