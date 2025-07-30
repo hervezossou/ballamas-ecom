7112d39780bc045b4d35946ce54d91c7c3f682ad
Hervé Zossou <hervezossou8@gmail.com>
2025-07-30 11:53:40 +0100

    refactor(collectionPage): split collection page UI into a separated component and added metadata

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -1,55 +1,11 @@
-"use client";
+import { CollectionsPage } from "@/components/pages/CollectionsPage";
+import { Metadata } from "next";
 
-import { useState, useEffect, use } from "react";
-import { CollectionCard } from "@/components/molecules/CollectionCard";
-import { CollectionCardSkeleton } from "@/components/skeletons/CollectionCardSkeleton";
-import { Collection } from "@/types";
-import { getAllCollections } from "@/lib/products";
+export const metadata: Metadata = {
+   title: "Our Collections",
+   description: "Discover all our collections of products.",
+};
 
-export default function CollectionsPage() {
-   const [collections, setCollections] = useState<Collection[]>([]);
-   const [isLoading, setIsLoading] = useState(true);
-
-   useEffect(() => {
-      const fetchCollections = async () => {
-         try {
-            setIsLoading(true);
-            const fetchedCollections = await getAllCollections(10);
-            if (fetchedCollections) {
-               setCollections(fetchedCollections || []);
-            }
-         } catch (error) {
-            console.error("Error fetching collections:", error);
-         } finally {
-            setIsLoading(false);
-         }
-      };
-
-      fetchCollections();
-   }, []);
-
-   return (
-      <>
-         <div className="mt-8 w-full max-w-[1200px] flex flex-col items-start justify-between px-10">
-            <h1 className="font-archivo font-bold text-2xl lg:text-4xl">
-               Collections
-            </h1>
-            <p className="font-archivo text-sm md:text-base lg:text-lg">
-               Explore our diverse collections of products.
-            </p>
-         </div>
-         <div className="mx-auto w-full max-w-[1200px] grid grid-cols-1 items-center justify-center gap-4 py-12 md:grid-cols-2 md:gap-8 lg:grid-cols-3 lg:gap-12 lg:py-16">
-            {isLoading
-               ? Array.from({ length: 8 }).map((_, index) => (
-                    <CollectionCardSkeleton key={index} />
-                 ))
-               : collections.map((collection) => (
-                    <CollectionCard
-                       key={collection.handle}
-                       collection={collection}
-                    />
-                 ))}
-         </div>
-      </>
-   );
+export default function Page() {
+   return <CollectionsPage />;
 }
