e2eec14b2b98c35c6d8e8db9707164be85108bfd
Hervé Zossou <hervezossou8@gmail.com>
2025-08-01 14:05:21 +0100

    update(ProductPage): added YouMayAlsoLike section

--- src/components/pages/ProductPage.tsx
+++ src/components/pages/ProductPage.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ProductDetailsWrapper } from "@/components/organisms/ProductDetailsWrapper";
-import { CollectionProducts } from "@/components/organisms/CollectionProducts";
+import { YouMayAlsoLike } from "@/components/organisms/YouMayAlsoLike";
 
 interface ProductPageProps {
    handle: string;
@@ -16,14 +16,7 @@ export default function ProductPage({ handle }: ProductPageProps) {
          <section className="w-full flex items-center justify-center pt-10 lg:pt-16">
             <ProductDetailsWrapper handle={productHandle} />
          </section>
-         <section className="w-full max-w-[1200px] flex flex-col items-start justify-between gap-4 py-8 lg:py-12 lg:gap-8">
-            <h2 className="font-chillax font-semibold text-2xl lg:text-3xl">
-               You may also like
-            </h2>
-            <div className="w-full flex self-stretch flex-col items-center justify-between gap-2 md:flex-row md:overflow-hidden lg:gap-3.5">
-               <CollectionProducts />
-            </div>
-         </section>
+         <YouMayAlsoLike handle={productHandle} />
       </>
    );
 }
