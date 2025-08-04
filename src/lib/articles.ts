1bea1a493bbd4628a56a0fa4ea12da1fee3e3a07
Hervé Zossou <hervezossou8@gmail.com>
2025-08-04 19:38:53 +0100

    update(lib): created a file for all articles related request functions

---
+++ src/lib/articles.ts
@@ -0,0 +1,36 @@
+import { Article } from "@/types";
+import { graphqlClient } from "./graphqlClient";
+import { gql } from "graphql-request";
+
+export const getAllArticles = async (
+   limit: number = 6
+): Promise<Article[] | null> => {
+   const query = gql`
+      query GetAllArticles($limit: Int!) {
+         articles(first: $limit, reverse: true) {
+            edges {
+                node {
+                    id
+                    handle
+                    title
+                    publishedAt
+                    excerpt
+                    contentHtml
+                }
+            }
+         }
+      }
+   `;
+
+   try {
+      const variables = { limit };
+      const response = await graphqlClient.request<{
+         articles: { edges: { node: Article }[] };
+      }>(query, variables);
+
+      return response.articles.edges.map((edge) => edge.node);
+   } catch (error) {
+      console.error("Failed to fetch articles:", error);
+      return null;
+   }
+};
