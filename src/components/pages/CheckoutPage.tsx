a83b428b69c2fe774cea58c1a2ca136644511b76
Hervé Zossou <hervezossou8@gmail.com>
2025-08-08 12:36:22 +0100

    fix(checkout): fixed h-scren by h-screen

--- src/components/pages/CheckoutPage.tsx
+++ src/components/pages/CheckoutPage.tsx
@@ -1,19 +1,20 @@
 import { OrderBlock } from "../organisms/OrderBlock";
 import { ShippingMethods } from "../organisms/ShippingMethods";
+import { PaymentForm } from "../organisms/PaymentForm";
 
 export const CheckoutPage = () => {
    return (
-      <div className="w-full min-h-scren pt-4 pb-8 md:pt-8 lg:pt-12 xl:pb-24">
+      <div className="w-full min-h-screen pt-4 pb-8 md:pt-8 lg:pt-12 xl:pb-24">
          <section className="flex flex-col items-start gap-5">
             <h3 className="font-chillax font-semibold text-lg md:text-2xl">
                Checkout
             </h3>
-            <div className="w-full flex flex-col items-start justify-between gap-15.5 xl:flex-row xl:gap-28">
+            <div className="w-full flex flex-col items-start justify-between gap-15.5 lg:flex-row xl:gap-28">
                <div className="w-full flex flex-col items-start justify-between gap-4">
                   <OrderBlock />
                   <ShippingMethods />
                </div>
-               <div></div>
+               <PaymentForm />
             </div>
          </section>
       </div>
