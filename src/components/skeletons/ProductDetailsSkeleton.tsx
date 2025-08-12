02dc43c5b80d3aa240cb6c5624fda0c9dc4fa9f4
Hervé Zossou <hervezossou8@gmail.com>
2025-08-12 16:14:41 +0100

    feat(ProductDetailsSkeleton): reshape size for responsiveness

--- src/components/skeletons/ProductDetailsSkeleton.tsx
+++ src/components/skeletons/ProductDetailsSkeleton.tsx
@@ -1,40 +1,40 @@
 export const ProductDetailsSkeleton = () => {
    return (
-      <div className="flex flex-col items-start justify-center gap-8 animate-pulse lg:h-[600px] lg:flex-row lg:gap-10.5">
-         <div className="w-full bg-gray-300 h-[600px] rounded-4xl lg:w-[579px]"></div>
-         <div className="flex flex-col items-start justify-between gap-4 lg:w-1/2 lg:gap-5.5">
-            <div className="flex flex-col gap-4 h-[410px] lg:gap-5">
+      <div className="w-full flex flex-col items-start justify-center gap-8 animate-pulse lg:h-[600px] lg:flex-row lg:gap-10.5">
+         <div className="w-full h-80 bg-gray-300 rounded-4xl lg:w-1/2 lg:h-full"></div>
+         <div className="w-full flex flex-col items-start justify-between gap-4 lg:w-1/2 lg:gap-5.5">
+            <div className="w-full flex flex-col gap-4 h-max lg:h-[410px] lg:gap-5">
                <div className="flex flex-col gap-2 md:gap-3 lg:gap-4.5">
-                  <div className="h-10 w-3/4 bg-gray-300 rounded" />
-                  <div className="h-10 w-1/4 bg-gray-300 rounded"></div>
+                  <div className="w-3/4 h-6 bg-gray-300 rounded-lg lg:h-10" />
+                  <div className="w-1/4 h-6 bg-gray-300 rounded-lg lg:h-10"></div>
                   <div className="flex flex-col gap-2 lg:gap-3">
-                     <div className="w-40 h-8 bg-gray-300 rounded"></div>
+                     <div className="w-40 h-5 bg-gray-300 rounded-lg lg:h-8"></div>
                      <div className="flex items-center justify-start gap-4">
-                        <div className="bg-gray-300 size-8 rounded-full"></div>
-                        <div className="bg-gray-300 size-8 rounded-full"></div>
-                        <div className="bg-gray-300 size-8 rounded-full"></div>
-                        <div className="bg-gray-300 size-8 rounded-full"></div>
-                        <div className="bg-gray-300 size-8 rounded-full"></div>
+                        <div className="bg-gray-300 size-6 rounded-full lg:size-8"></div>
+                        <div className="bg-gray-300 size-6 rounded-full lg:size-8"></div>
+                        <div className="bg-gray-300 size-6 rounded-full lg:size-8"></div>
+                        <div className="bg-gray-300 size-6 rounded-full lg:size-8"></div>
+                        <div className="bg-gray-300 size-6 rounded-full lg:size-8"></div>
                      </div>
                   </div>
                   <div className="flex flex-col gap-2 lg:gap-3">
-                     <div className="w-28 h-8 bg-gray-300 rounded"></div>
+                     <div className="w-28 h-5 bg-gray-300 rounded-lg lg:h-8"></div>
                      <div className="flex items-center gap-2">
-                        <div className="w-20 h-10 bg-gray-300 rounded-4xl"></div>
-                        <div className="w-20 h-10 bg-gray-300 rounded-4xl"></div>
-                        <div className="w-20 h-10 bg-gray-300 rounded-4xl"></div>
-                        <div className="w-20 h-10 bg-gray-300 rounded-4xl"></div>
+                        <div className="w-16 h-10 bg-gray-300 rounded-4xl"></div>
+                        <div className="w-16 h-10 bg-gray-300 rounded-4xl"></div>
+                        <div className="w-16 h-10 bg-gray-300 rounded-4xl"></div>
+                        <div className="w-16 h-10 bg-gray-300 rounded-4xl"></div>
                      </div>
                   </div>
                </div>
-               <div className="flex w-[579px] items-center gap-2">
-                  <div className="bg-gray-300 w-1/2 h-16 rounded-4xl"></div>
-                  <div className="bg-gray-300 w-1/2 h-16 rounded-4xl"></div>
+               <div className="w-full max-w-[579px] flex items-center gap-2">
+                  <div className="w-1/2 h-12 bg-gray-300 rounded-4xl lg:h-16"></div>
+                  <div className="w-1/2 h-12 bg-gray-300 rounded-4xl lg:h-16"></div>
                </div>
             </div>
             <div className="w-full flex flex-col items-start justify-start gap-2 lg:gap-3.5">
-               <div className="bg-gray-300 w-1/3 h-8 rounded"></div>
-               <div className="bg-gray-300 w-full h-30 rounded"></div>
+               <div className="w-1/3 h-8 bg-gray-300 rounded-lg"></div>
+               <div className="w-full h-40 bg-gray-300 rounded-lg lg:h-30"></div>
             </div>
          </div>
       </div>
